924fac0221c0089d804be61eebe7a4eb6d9d8bf8
CHHAILENG TIM <80568234+ChhailengTim@users.noreply.github.com>
2022-10-15 10:41:31 -0700

    Add files via upload

---
+++ test/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'widgets/home_widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String str = "Phnom Penh";
+
+    return Scaffold(
+      appBar: HomeWidgets.instance.appBarWidgets(str),
+      body: ListView(
+        children: [
+          HomeWidgets.instance.headWidgets(),
+          HomeWidgets.instance.sliderWidgets(),
+          HomeWidgets.instance.wrapIcons(),
+          HomeWidgets.instance.giftWidgets(),
+        ],
+      ),
+    );
+  }
+}
